99b32a5aa4ae91cf63e1eacb35bc827f62728d27
Nikita Drozhzhin <nikitadrzh@gmail.com>
2018-01-24 14:51:35 +0300

    add String for pullRequestTest

--- app/src/main/java/ru/nikitadrzh/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/ru/nikitadrzh/photogallery/PhotoGalleryActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 public class PhotoGalleryActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
+        String pullRequestTest = "deleteLater";
+
         return PhotoGalleryFragment.newInstance();
     }
     //1-наслед. от SingleFragActiv, создаем контейнер(activ.fragment.xml)//
