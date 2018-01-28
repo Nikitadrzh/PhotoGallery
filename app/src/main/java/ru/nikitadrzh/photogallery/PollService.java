f7f03e7a7b9a82c94ae2dd9e75b7bc5639377b55
Nikita Drozhzhin <nikitadrzh@gmail.com>
2018-01-28 19:28:59 +0300

    Добавлены notifications, но требуется рефакторинг для oreo

--- app/src/main/java/ru/nikitadrzh/photogallery/PollService.java
+++ app/src/main/java/ru/nikitadrzh/photogallery/PollService.java
@@ -3,12 +3,17 @@ package ru.nikitadrzh.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -21,6 +26,7 @@ import java.util.concurrent.TimeUnit;
 public class PollService extends IntentService {//служба опроса
     private static final String TAG = "PollService";//константа для debugger
     private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);//60 сек
+    private static final String NOTIFICATION_CHANNEL_0 = "new_pictures_channel";
 
     public PollService() {
         super(TAG);
@@ -60,6 +66,8 @@ public class PollService extends IntentService {//служба опроса
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {//вызывается, когда подходит очередь
+        NotificationManager notificationManager = null;
+
         if (!isNetworkAvailableAndConnected()) {//выход из метода при отсутствии сети
             return;
         }
@@ -84,6 +92,36 @@ public class PollService extends IntentService {//служба опроса
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                notificationManager =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                String channelId = "new_photo_channel_notification";
+                CharSequence channelName = "New Photo Channel";
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel notificationChannel =
+                        new NotificationChannel(channelId, channelName, importance);
+                if (notificationManager != null) {
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+            }
+
+            Notification notification = new NotificationCompat
+                    .Builder(this, "new_photo_channel_notification")
+                    .setTicker(getResources().getString(R.string.new_pictures_title))//бегущая
+                    // строка
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)//системная иконка
+                    .setContentTitle(getResources().getString(R.string.new_pictures_title))
+                    .setContentText(getResources().getString(R.string.new_pictures_text))
+                    .setContentIntent(PendingIntent.getActivity(this, 0,
+                            PhotoGalleryActivity.newIntent(this), 0))
+                    .setAutoCancel(true)
+                    .build();
+//            NotificationManagerCompat.from(this).notify(0, notification);//создается
+//            // notificationManager и отправляется notification
+            if (notificationManager != null) {
+                notificationManager.notify(0, notification);
+            }
         }
 
         QueryPreferences.setLastResultId(this, resultId);//сохраняем в общих настройках
