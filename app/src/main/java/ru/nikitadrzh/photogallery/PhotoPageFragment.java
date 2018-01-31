d477e06b6e499a6ff5db6f7d34b8f4f0e8b0da52
Nikita Drozhzhin <nikitadrzh@gmail.com>
2018-01-31 19:16:13 +0300

    Улучшен webView

--- app/src/main/java/ru/nikitadrzh/photogallery/PhotoPageFragment.java
+++ app/src/main/java/ru/nikitadrzh/photogallery/PhotoPageFragment.java
@@ -1,12 +1,17 @@
 package ru.nikitadrzh.photogallery;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by Nekit on 31.01.2018.
@@ -17,6 +22,7 @@ public class PhotoPageFragment extends VisibleFragment {
 
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static PhotoPageFragment newInstance(Uri uri) {//создаем фрагмент через этот метод
         Bundle args = new Bundle();
@@ -32,12 +38,33 @@ public class PhotoPageFragment extends VisibleFragment {
         mUri = getArguments().getParcelable(ARG_URI);//получаем URI
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_page, container, false);
+        mProgressBar = view.findViewById(R.id.progress_bar);
+        mProgressBar.setMax(100);
         mWebView = view.findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(mUri.toString());
         return view;
     }
 }
