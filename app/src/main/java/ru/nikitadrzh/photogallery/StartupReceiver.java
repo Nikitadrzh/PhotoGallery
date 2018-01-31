10d4f5cb709ab916fe23cd99d47f1ea32de1486a
Nikita Drozhzhin <nikitadrzh@gmail.com>
2018-01-31 11:27:17 +0300

    Создан StartupReceiver и настроен на BOOT_COMPLETED

---
+++ app/src/main/java/ru/nikitadrzh/photogallery/StartupReceiver.java
@@ -0,0 +1,20 @@
+package ru.nikitadrzh.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Nekit on 31.01.2018.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {//широковещательный Приемник
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());//тут action -
+        // BOOT_COMPLETED из манифеста
+    }
+}
